ce37ee2360c87c1d7939f7b300685879f4ab3ddf
Laurent Mwanamputu <laurentmwn@gmail.com>
2025-05-09 11:18:07 +0100

    view: create page me, course-info

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,15 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/course-info', function () {
+    return Inertia::render('course-info');
+})->name('course');
+
+
+Route::get('/me', function () {
+    return Inertia::render('me');
+})->name('me');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
 });
